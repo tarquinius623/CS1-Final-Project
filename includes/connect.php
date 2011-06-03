1f4b5e8c69f00f7dc6190d667ebb82d8f982bfc3
tarquinius623 <blichtman@friendscentral.org>
2011-06-03 07:09:56 -0700

    Edited includes/connect.php via GitHub

--- includes/connect.php
+++ includes/connect.php
@@ -7,9 +7,9 @@
 
 //Development server: orchestra.io
 $server = "10.194.111.8";
-$username = "user_83da255f";
-$password = "&rQxV-U4%wFYQv";
-$database = "db_83da255f";
+$username = "user_3b905224";
+$password = "n2UQbFy%K3r&vn";
+$database = "db_3b905224";
 
 mysql_connect($server, $username, $password) or die ("Could not connect to server");
 mysql_select_db($database) or die ("Could not connect to database");
